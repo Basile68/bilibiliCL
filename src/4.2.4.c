f9f6d7d46e12ec9ca8ca30c6a9e91bf06f4ff316
may68-git <mayjune0909@foxmail.com>
2026-01-13 01:55:01 +0800

    1:54

--- src/4.2.4.c
+++ src/4.2.4.c
@@ -9,10 +9,10 @@ int main()
 	int ret = 0;
 	
 	while (x > 0){
-	digit = x%10;
-	ret = ret * 10 + digit;
-	printf ("x=%d, digit=%d, ret=%d\n", x, digit, ret);
-	x /= 10;
+		digit = x%10;
+		ret = ret * 10 + digit;
+		printf ("x=%d, digit=%d, ret=%d\n", x, digit, ret);
+		x /= 10;
 	}
 	
 	printf("%d", ret);
